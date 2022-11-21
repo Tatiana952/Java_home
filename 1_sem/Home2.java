8f2ce356d2ff2dab39e65446b9f03627ba22fd1d
Tatiana <lulu.kep@yandex.ru>
2022-11-22 00:58:12 +0400

    Home2_2

--- 1_sem/Home2.java
+++ 1_sem/Home2.java
@@ -4,10 +4,11 @@ public class Home2 {
     public static void main(String[] args) {
         int count = 0;
         for (int i = 1; i < 1001; i++) {
-            for (int j = 1; j <= i; j++) if (i % j == 0) ++count;
+            for (int j = 1; j <= i; j++) {
+                Sif (i % j == 0) ++count;
+            }
             if (count == 2) System.out.println(i);
             count = 0;
-Ы
         }
     }
 }
