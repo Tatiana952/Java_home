ef422a1d0a350aad02a9e10fda39df6cda19b173
Tatiana <lulu.kep@yandex.ru>
2022-11-22 00:59:04 +0400

    Home2_3

--- 1_sem/Home2.java
+++ 1_sem/Home2.java
@@ -1,11 +1,9 @@
-import java.util.Scanner;
-
 public class Home2 {
     public static void main(String[] args) {
         int count = 0;
         for (int i = 1; i < 1001; i++) {
             for (int j = 1; j <= i; j++) {
-                Sif (i % j == 0) ++count;
+                if (i % j == 0) ++count;
             }
             if (count == 2) System.out.println(i);
             count = 0;
