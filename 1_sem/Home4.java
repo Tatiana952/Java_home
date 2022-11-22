1c40b0a8b95029052ac31ac5e9e515af77f153e9
Tatiana <lulu.kep@yandex.ru>
2022-11-22 17:07:22 +0400

    Home4_2

--- 1_sem/Home4.java
+++ 1_sem/Home4.java
@@ -3,8 +3,45 @@ import java.util.Scanner;
 public class Home4 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter an equation like (3?+?6=56): ");
+        System.out.print("Enter an equation like (3? + ?6 = 56): ");
         String eq = s.nextLine();
-        
+        s.close();
+        eq = eq.replaceAll(" ","");
+        int ind1 = eq.indexOf("+");
+        int ind2 = eq.indexOf("=");
+        String a = "";
+        String b = "";
+        String res = "";
+        for (int i = 0; i < ind1; i++) {
+            a += eq.charAt(i);
+        }
+        for (int i = ind1+1; i < ind2; i++) {
+            b += eq.charAt(i);
+        }
+        for (int i = ind2+1; i < eq.length(); i++) {
+            res += eq.charAt(i);
+        }
+        int res_t;
+        int count = 0;
+        String a2 = null, b2 = null;
+        int l = 0,r = 0;
+        if(a.indexOf("?") == 0) l = 1;
+        if(b.indexOf("?") == 0) r = 1;
+        calcul:
+            for (int i = 0 + l; i < 10; i++) {
+                a2 = a.replace("?", String.format("%d",i));
+                for (int j = 0 + r; j < 10; j++) {
+                    b2 = b.replace("?", String.format("%d",j));
+                    res_t = Integer.parseInt(a2) + Integer.parseInt(b2);
+//                    System.out.printf("%d + %d = %d\n", Integer.parseInt(a2), Integer.parseInt(b2), res_t);
+                    if (res_t == Integer.parseInt(res)) {
+                        break calcul;
+                    }else count++;
+                }
+            }
+        if (count == 100) System.out.println("Combination not found");
+        else if ((a.length() == 1 || b.length() == 1) && count == 90) System.out.println("Combination not found");
+        else if ((a.length() == 1 && b.length() == 1) && count == 81) System.out.println("Combination not found");
+        else System.out.printf("Combination: %d + %d = %d", Integer.parseInt(a2), Integer.parseInt(b2), Integer.parseInt(res));
     }
 }
