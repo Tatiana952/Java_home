d07122d00f1e28bca55a2939f0ac1d68be738048
Tatiana <lulu.kep@yandex.ru>
2022-11-22 14:47:36 +0400

    Home3

---
+++ 1_sem/Home3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Home3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x = ");
+        float x = sc.nextFloat();
+        System.out.print("Enter y = ");
+        float y = sc.nextFloat();
+        System.out.print("Enter an operator(like: +, -, *, /): ");
+        String z = sc.next();
+        switch (z){
+            case "+":
+                System.out.printf("Result: x + y = %.2f + %.2f = %.3f\n", x, y, x+y);
+                break;
+            case "-":
+                System.out.printf("Result: x - y = %.2f - %.2f = %.3f\n", x, y, x-y);
+                break;
+            case "*":
+                System.out.printf("Result: x * y = %.2f * %.2f = %.3f\n", x, y, x*y);
+                break;
+            case "/":
+                System.out.printf("Result: x / y = %.2f / %.2f = %.3f\n", x, y, x/y);
+                break;
+        }
+    }
+}
