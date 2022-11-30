1da407d63065ad9237f3abfed34bd6d32e5e2b0f
Tatiana952 <107501679+Tatiana952@users.noreply.github.com>
2022-11-30 18:46:49 +0400

    h1

--- 2_sem/home1.java
+++ 2_sem/home1.java
@@ -1,11 +1,9 @@
-package sem2;
+//package 2_sem;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class home1 {
     public static void main(String[] args) {
@@ -15,15 +13,22 @@ public class home1 {
             Reader reader = new FileReader("Home1.json");
             JSONParser parser = new JSONParser();
             JSONObject data = (JSONObject)parser.parse(reader);
-//            System.out.println(data);
+            String[] c = data.toString().replaceAll("\"", "").replaceAll("\\{", "").replaceAll("}", "").split(",");
+            StringBuilder res = new StringBuilder("select * from students where");
+
+            for (int i = 0; i < c.length; i++) {
+
+                if (c[i].contains("null") == false) {
+                    c[i] = c[i].replace(":", " = '") + "'";
+                    res.append(" " + c[i] + " and");
+                }
+            }
+            System.out.println(res.substring(0, res.length()-4) + ";");
         }catch(IOException e) {
             e.printStackTrace();
         }catch (ParseException parseE){
             parseE.printStackTrace();
         }
-
-        
-
 
     }
     public static void Create(){
@@ -32,7 +37,7 @@ public class home1 {
         jsonchick.put("country","Russia");
         jsonchick.put("city","Moscow");
         jsonchick.put("age","null");
-        System.out.println(jsonchick);
+//        System.out.println(jsonchick);
 
         try{
             FileWriter file = new FileWriter(new File("Home1.json"));
@@ -42,7 +47,5 @@ public class home1 {
             e.printStackTrace();
         }
         System.out.println("JSON file Home1.json created");
-
-
     }
 }
